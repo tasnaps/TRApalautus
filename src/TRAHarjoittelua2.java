7a381fe65af60a63b0d8ed26199c2960d607459c
tasnaps <74553493+tasnaps@users.noreply.github.com>
2021-09-16 20:08:12 +0300

    asdasd

---
+++ src/TRAHarjoittelua2.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class TRAHarjoittelua2 {
+    /**
+     * Aikavaatimus:
+     * O(n**2)
+     * Koska: i.hasnext -> O(n)
+     * ja if haaran B.contains(i.next()) -> (n)
+     * If body on vaan O(1)
+     *
+     * Aikavaatimusta voisi parantaa muuttamalla while loopin if haaraa siten
+     * että ei aina käydä B listaa alusta lähtien <- B.contains(i.next())
+     *
+     * Opiskelijan kannalta hyvä algorimti, sillä pätee molempiin tehtäviin: 8 ja 9
+     *
+     *
+     */
+
+    public static int kasvavienErotus (LinkedList<Integer> A, LinkedList<Integer> B){
+        Iterator<Integer> i = A.iterator();
+        int summa = 0;
+
+        while(i.hasNext()){
+            if(B.contains(i.next())){
+                i.remove();
+                summa++;
+            }
+
+        }
+        return summa;
+    }
+
+     static LinkedList algo(LinkedList LL) {
+        while (LL.size() > 0)
+            LL.remove(0);
+        return LL;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> A = new LinkedList<Integer>();
+        A.add(1);
+        A.add(2);
+        A.add(3);
+        LinkedList<Integer> B = new LinkedList<Integer>();
+        B.add(1);
+        B.add(2);
+        B.add(3);
+        B.add(4);
+        int poisto =kasvavienErotus(A,B);
+        System.out.println(poisto);
+        System.out.println(algo(A));
+
+    }
+}
---
+++ src/TRAHarjottelua.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class TRAHarjottelua {
+
+        public static int poistot(ArrayList<Integer> A, ArrayList<Integer> B){
+            /**
+             * Homman nimi: Palauta monta kertaa poistetaan listasta A alkiot jotka esiintyvät listassa B.
+             *
+             * aikavaatimus:
+             *      * O(n**2)
+             *      * Koska: i.hasnext -> O(n)
+             *      * ja if haaran B.contains(i.next()) -> (n)
+             *      * If body on vaan O(1)
+             *      *
+             *      * Aikavaatimusta voisi parantaa muuttamalla while loopin if haaraa siten
+             *      * että ei aina käydä B listaa alusta lähtien <- B.contains(i.next())
+             *
+             */
+            int palautus = 0;
+            Iterator<Integer> i = A.iterator();
+            while(i.hasNext()){
+                if(B.contains(i.next())){
+                    i.remove();
+                    palautus++;
+                }
+            }
+            return palautus;
+
+
+
+
+        }
+
+
+
+    public static void main(String[] args) {
+            ArrayList<Integer> A = new ArrayList<Integer>();
+            A.add(1);
+            A.add(2);
+            A.add(3);
+            ArrayList<Integer> B = new ArrayList<Integer>();
+            B.add(1);
+            B.add(2);
+            B.add(3);
+            B.add(4);
+            int poisto =poistot(A,B);
+            System.out.println(poisto);
+
+    }
+}
