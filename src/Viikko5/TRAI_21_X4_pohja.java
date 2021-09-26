27f99791f81f484dae9cf04a146bd34b7d273c8e
tasnaps <74553493+tasnaps@users.noreply.github.com>
2021-09-26 11:38:16 +0300

    Valmiiiis!!!

--- src/Viikko5/TRAI_21_X4_pohja.java
+++ src/Viikko5/TRAI_21_X4_pohja.java
@@ -11,6 +11,8 @@ public class TRAI_21_X4_pohja implements TRAI_21_X4 {
 
     /**
      * ITSEARVIOINTI TÃ„HÃ„N:
+     *  5/5 Hirveästi piti debugata että sain oikean vastauksen.
+     *  Aikavaativuus: O(
      *
      *
      *
@@ -36,54 +38,31 @@ public class TRAI_21_X4_pohja implements TRAI_21_X4 {
 
     }
 
-    /**
-     * Palauttaa binÃ¤Ã¤ripuun solmun n edeltÃ¤jÃ¤n sisÃ¤jÃ¤rjestyksessÃ¤.
-     *
-     * @param n BinÃ¤Ã¤ripuun solmu.
-     * @return edeltÃ¤jÃ¤solmu tai null jollei edeltÃ¤jÃ¤Ã¤ ole.
-     */
     @Override
     public BTreeNode sisaEdellinen(BTreeNode n) {
         BTreeNode vastaus = null;
-
-        // Nämä if lauseet toimivat.
-        //Mikäli lapsen oikea lapsi on olemassa:
-        if (n.getLeftChild() != null && n.getLeftChild().getRightChild() != null) {
-            vastaus = n.getLeftChild().getRightChild();
+        //Edeltäjää etsitään vasemman lapsen kaikkein oikeimmasta päästä tämä ei toimi.
+        if (n.getLeftChild() != null) {//n.getLeftChild().getRightChild() != null
+            n = n.getLeftChild();
+            while(n.getRightChild() != null){
+                n = n.getRightChild();
+            }
+            vastaus = n;
             return vastaus;
         }
+        //nämä else haarat etsii missä kohti haaraudutaan oikealle
         else if (n.getParent() != null && n.getParent().getRightChild() != null && n.getParent().getRightChild().equals(n)) {
             vastaus = n.getParent();
             return vastaus;
-        }
-
-        //----------------------------------------------------------------------------------------------------------------
-        //MIkäli n on vanhemman vasen lapsi
-        //tee valmiiksi: palauta solmun edeltäjä joka on esivanhempi, jonka oikeassa haarassa solmu on
-        else if (n.getParent() != null && n.getParent().getLeftChild() != null && n.getParent().getLeftChild().equals(n)) {
-
-
-
-            // tämä if aiheuttaa kierron
-
-            if(n.getParent() != null && n.getParent().getParent() != null && n.getParent().getParent().getRightChild().equals(n.getParent())){
-                vastaus = n.getParent().getParent().getRightChild();
-                return vastaus;
+        }else{
+            while(n.getParent() != null){
+                n = n.getParent();
+                if(n.getParent()!= null && n.getParent().getRightChild()!=null &&n.getParent().getRightChild().equals(n)){
+                    vastaus = n.getParent();
+                    return vastaus;
+                }
             }
-
             return vastaus;
         }
-
-        return vastaus;
-        // testauksen ajaksi tämä on kommentoitu poist tieltä
-                        /**
-                         * while (n.getParent() != null && n.getParent().getParent() != null &&n.getParent().getParent().getRightChild() == null) {
-                         *                 n = n.getParent();
-                         *                 System.out.println("loopissa");
-                         *             }
-                         *
-                         *
-                         */
-
     }
 }
